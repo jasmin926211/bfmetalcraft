cb9582c8f03ff1b84e8ed7778adce86ec9d45011
jasmin926211 <jjt926211@gmail.com>
2023-09-02 17:05:15 +0530

    First commit for bf metal craft

---
+++ js/mapcode.js
@@ -0,0 +1,22 @@
+function initialize() {
+  'use strict';
+  var mapOptions = {
+    zoom: 15,
+    scrollwheel: false,
+    center: new google.maps.LatLng(21.17024, 72.831061),
+  };
+
+  var map = new google.maps.Map(
+    document.getElementById('googleMap'),
+    mapOptions
+  );
+
+  var marker = new google.maps.Marker({
+    position: map.getCenter(),
+    animation: google.maps.Animation.BOUNCE,
+    icon: 'img/logo/map-marker.png',
+    map: map,
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
